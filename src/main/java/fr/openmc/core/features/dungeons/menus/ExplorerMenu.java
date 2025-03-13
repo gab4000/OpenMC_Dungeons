7d154fac5be10b4e697f1431037b349bca5b64e5
gab4000 <gabriel.cosme@orange.fr>
2025-03-13 18:49:31 +0100

    resolve errors 2

--- src/main/java/fr/openmc/core/features/dungeons/menus/ExplorerMenu.java
+++ src/main/java/fr/openmc/core/features/dungeons/menus/ExplorerMenu.java
@@ -25,151 +25,153 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import static fr.openmc.core.features.dungeons.data.DungeonManager.*;
+import static fr.openmc.core.features.dungeons.data.DungeonManager.config;
+import static fr.openmc.core.features.dungeons.data.DungeonManager.saveReloadConfig;
 
 public class ExplorerMenu extends Menu {
-
-    @Getter
-    public static Map<UUID, Integer> dungeonSoloCondition = new HashMap<>();
-
-    public static OMCPlugin plugin;
-    Player player;
-
-
-    public ExplorerMenu(Player player) {
-        super(player);
-        this.player = player;
-    }
-
-    @Override
-    public @NotNull String getName() {
-        return PlaceholderAPI.setPlaceholders(player, "§r§f%img_offset_-8%%img_test%");
-    }
-
-    @Override
-    public @NotNull InventorySize getInventorySize() {
-        return InventorySize.NORMAL;
-    }
-
-    @Override
-    public void onInventoryClick(InventoryClickEvent inventoryClickEvent) {}
-
-    @Override
-    public @NotNull Map<Integer, ItemStack> getContent() {
-        Map<Integer, ItemStack> map = new HashMap<>();
-
-        map.put(13, new ItemBuilder(this, Material.PAPER, itemMeta -> {
-            itemMeta.setDisplayName(DungeonList.dungeon_training.getDungeonName());
-        }).setOnClick(inventoryClickEvent -> {
-            tpAvailableDungeon(player, DungeonList.dungeon_training);
-            getOwner().closeInventory();
-        }));
-
-        map.put(15, new ItemBuilder(this, Material.PAPER, itemMeta -> {
-            itemMeta.setDisplayName("Dungeons");
-        }).setNextMenu(new DungeonsChoiceMenu(player)));
-
-        return map;
-    }
-
-    public static void tpAvailableDungeon(Player player, DungeonList dungeons) {
-
-        if (config.getConfigurationSection("dungeon." + "dungeon_places.")==null){
-            MessagesManager.sendMessage(player, Component.text("§4Erreur lors de la téléportation vers le donjon"), Prefix.DUNGEON, MessageType.ERROR, false);
-            return;
-        }
-
-        if (config.getConfigurationSection("dungeon." + "team.")!=null && !dungeons.equals(DungeonList.dungeon_training)){
-            for (String team : config.getConfigurationSection("dungeon." + "team.").getKeys(false)){
-                String basePath = "dungeon." + "team." + team;
-                if (config.getStringList(basePath + ".player_in_team").contains(player.getName())){
-                    if (team.equals(player.getName())) {
-                        for (String dungeon : config.getConfigurationSection("dungeon." + "dungeon_places." + dungeons + ".").getKeys(false)){
-                            String path = "dungeon." + "dungeon_places." + dungeons + "." + dungeon;
-
-                            boolean available = config.getBoolean(path + ".available");
-                            String location = config.getString(path + ".spawn_co");
-
-                            if (location != null && available) {
-
-                                String[] parts = location.split(",");
-
-                                if (parts.length >= 3) {
-                                    double x = Double.parseDouble(parts[0]);
-                                    double y = Double.parseDouble(parts[1]);
-                                    double z = Double.parseDouble(parts[2]);
-
-                                    if (Bukkit.getWorld("Dungeons") != null) {
-                                        config.set(path + ".available", false);
-                                        for (String playerInTeam : config.getStringList(basePath + ".player_in_team")){
-                                            Player teamMate = Bukkit.getPlayer(playerInTeam);
-                                            Location tp = new Location(Bukkit.getWorld("Dungeons"), x, y, z);
-                                            String dungeonPath = "dungeon." + "players_in_dungeon." + teamMate.getName();
-                                            teamMate.teleport(tp);
-                                            config.set(dungeonPath + ".dungeon", String.valueOf(dungeons));
-                                            config.set(dungeonPath + ".places", dungeon);
-                                            config.set(dungeonPath + ".states", "alive");
-                                            config.set(basePath + ".remain", dungeons.getKillToFinishCondition());
-                                            Chronometer.start(teamMate, "dungeons", dungeons.getTime(), ChronometerType.ACTION_BAR, "temps : %sec%", ChronometerType.ACTION_BAR, "%null%");
-                                        }
-                                        saveReloadConfig();
-                                    } else {
-                                        MessagesManager.sendMessage(player, Component.text("§4Erreur"), Prefix.DUNGEON, MessageType.ERROR, false);
-                                    }
-                                    break;
-                                }
-                            }
-
-                            if (location == null) {
-                                plugin.getLogger().info("§4ERROR : a location in the dungeon.yml incorrectly initialized or right");
-                            }
-                        }
-                    } else {
-                        MessagesManager.sendMessage(player, Component.text("§4Vous n'êtes pas le chef de votre team"), Prefix.DUNGEON, MessageType.ERROR, false);
-                    }
-                    return;
-                }
-            }
-        }
-
-        for (String dungeon : config.getConfigurationSection("dungeon." + "dungeon_places." + dungeons + ".").getKeys(false)){
-            String path = "dungeon." + "dungeon_places." + dungeons + "." + dungeon;
-
-            boolean available = config.getBoolean(path + ".available");
-            String location = config.getString(path + ".spawn_co");
-
-            if (location != null && available) {
-
-                String[] parts = location.split(",");
-
-                if (parts.length >= 3) {
-                    double x = Double.parseDouble(parts[0]);
-                    double y = Double.parseDouble(parts[1]);
-                    double z = Double.parseDouble(parts[2]);
-
-                    if (Bukkit.getWorld("Dungeons") != null) {
-                        Location tp = new Location(Bukkit.getWorld("Dungeons"), x, y, z);
-                        String dungeonPath = "dungeon." + "players_in_dungeon." + player.getName();
-                        player.teleport(tp);
-                        config.set(path + ".available", false);
-                        config.set(dungeonPath + ".dungeon", String.valueOf(dungeons));
-                        config.set(dungeonPath + ".places", dungeon);
-                        config.set(dungeonPath + ".states", "alive");
-                        if (dungeons.getKillToFinishCondition()!=-1){
-                            dungeonSoloCondition.put(player.getUniqueId(), dungeons.getKillToFinishCondition());
-                        }
-                        Chronometer.start(player, "dungeons", dungeons.getTime(), ChronometerType.ACTION_BAR, "temps : %sec%", ChronometerType.ACTION_BAR, "%null%");
-                        saveReloadConfig();
-                    } else {
-                        MessagesManager.sendMessage(player, Component.text("§4Erreur"), Prefix.DUNGEON, MessageType.ERROR, false);
-                    }
-                    break;
-                }
-            }
-
-            if (location == null) {
-                plugin.getLogger().info("§4 ERROR : a location in the dungeon.yml incorrectly initialized or right");
-            }
-        }
-    }
+	
+	@Getter
+	public static Map<UUID, Integer> dungeonSoloCondition = new HashMap<>();
+	
+	public static OMCPlugin plugin;
+	Player player;
+	
+	
+	public ExplorerMenu(Player player) {
+		super(player);
+		this.player = player;
+	}
+	
+	public static void tpAvailableDungeon(Player player, DungeonList dungeons) {
+		
+		if (config.getConfigurationSection("dungeon." + "dungeon_places.") == null) {
+			MessagesManager.sendMessage(player, Component.text("§4Erreur lors de la téléportation vers le donjon"), Prefix.DUNGEON, MessageType.ERROR, false);
+			return;
+		}
+		
+		if (config.getConfigurationSection("dungeon." + "team.") != null && ! dungeons.equals(DungeonList.dungeon_training)) {
+			for (String team : config.getConfigurationSection("dungeon." + "team.").getKeys(false)) {
+				String basePath = "dungeon." + "team." + team;
+				if (config.getStringList(basePath + ".player_in_team").contains(player.getName())) {
+					if (team.equals(player.getName())) {
+						for (String dungeon : config.getConfigurationSection("dungeon." + "dungeon_places." + dungeons + ".").getKeys(false)) {
+							String path = "dungeon." + "dungeon_places." + dungeons + "." + dungeon;
+							
+							boolean available = config.getBoolean(path + ".available");
+							String location = config.getString(path + ".spawn_co");
+							
+							if (location != null && available) {
+								
+								String[] parts = location.split(",");
+								
+								if (parts.length >= 3) {
+									double x = Double.parseDouble(parts[0]);
+									double y = Double.parseDouble(parts[1]);
+									double z = Double.parseDouble(parts[2]);
+									
+									if (Bukkit.getWorld("Dungeons") != null) {
+										config.set(path + ".available", false);
+										for (String playerInTeam : config.getStringList(basePath + ".player_in_team")) {
+											Player teamMate = Bukkit.getPlayer(playerInTeam);
+											Location tp = new Location(Bukkit.getWorld("Dungeons"), x, y, z);
+											String dungeonPath = "dungeon." + "players_in_dungeon." + teamMate.getName();
+											teamMate.teleport(tp);
+											config.set(dungeonPath + ".dungeon", String.valueOf(dungeons));
+											config.set(dungeonPath + ".places", dungeon);
+											config.set(dungeonPath + ".states", "alive");
+											config.set(basePath + ".remain", dungeons.getKillToFinishCondition());
+											Chronometer.startChronometer(teamMate, "dungeons", dungeons.getTime(), ChronometerType.ACTION_BAR, "temps : %sec%", ChronometerType.ACTION_BAR, "%null%");
+										}
+										saveReloadConfig();
+									} else {
+										MessagesManager.sendMessage(player, Component.text("§4Erreur"), Prefix.DUNGEON, MessageType.ERROR, false);
+									}
+									break;
+								}
+							}
+							
+							if (location == null) {
+								plugin.getLogger().info("§4ERROR : a location in the dungeon.yml incorrectly initialized or right");
+							}
+						}
+					} else {
+						MessagesManager.sendMessage(player, Component.text("§4Vous n'êtes pas le chef de votre team"), Prefix.DUNGEON, MessageType.ERROR, false);
+					}
+					return;
+				}
+			}
+		}
+		
+		for (String dungeon : config.getConfigurationSection("dungeon." + "dungeon_places." + dungeons + ".").getKeys(false)) {
+			String path = "dungeon." + "dungeon_places." + dungeons + "." + dungeon;
+			
+			boolean available = config.getBoolean(path + ".available");
+			String location = config.getString(path + ".spawn_co");
+			
+			if (location != null && available) {
+				
+				String[] parts = location.split(",");
+				
+				if (parts.length >= 3) {
+					double x = Double.parseDouble(parts[0]);
+					double y = Double.parseDouble(parts[1]);
+					double z = Double.parseDouble(parts[2]);
+					
+					if (Bukkit.getWorld("Dungeons") != null) {
+						Location tp = new Location(Bukkit.getWorld("Dungeons"), x, y, z);
+						String dungeonPath = "dungeon." + "players_in_dungeon." + player.getName();
+						player.teleport(tp);
+						config.set(path + ".available", false);
+						config.set(dungeonPath + ".dungeon", String.valueOf(dungeons));
+						config.set(dungeonPath + ".places", dungeon);
+						config.set(dungeonPath + ".states", "alive");
+						if (dungeons.getKillToFinishCondition() != - 1) {
+							dungeonSoloCondition.put(player.getUniqueId(), dungeons.getKillToFinishCondition());
+						}
+						Chronometer.startChronometer(player, "dungeons", dungeons.getTime(), ChronometerType.ACTION_BAR, "temps : %sec%", ChronometerType.ACTION_BAR, "%null%");
+						saveReloadConfig();
+					} else {
+						MessagesManager.sendMessage(player, Component.text("§4Erreur"), Prefix.DUNGEON, MessageType.ERROR, false);
+					}
+					break;
+				}
+			}
+			
+			if (location == null) {
+				plugin.getLogger().info("§4 ERROR : a location in the dungeon.yml incorrectly initialized or right");
+			}
+		}
+	}
+	
+	@Override
+	public @NotNull String getName() {
+		return PlaceholderAPI.setPlaceholders(player, "§r§f%img_offset_-8%%img_test%");
+	}
+	
+	@Override
+	public @NotNull InventorySize getInventorySize() {
+		return InventorySize.NORMAL;
+	}
+	
+	@Override
+	public void onInventoryClick(InventoryClickEvent inventoryClickEvent) {
+	}
+	
+	@Override
+	public @NotNull Map<Integer, ItemStack> getContent() {
+		Map<Integer, ItemStack> map = new HashMap<>();
+		
+		map.put(13, new ItemBuilder(this, Material.PAPER, itemMeta -> {
+			itemMeta.setDisplayName(DungeonList.dungeon_training.getDungeonName());
+		}).setOnClick(inventoryClickEvent -> {
+			tpAvailableDungeon(player, DungeonList.dungeon_training);
+			getOwner().closeInventory();
+		}));
+		
+		map.put(15, new ItemBuilder(this, Material.PAPER, itemMeta -> {
+			itemMeta.setDisplayName("Dungeons");
+		}).setNextMenu(new DungeonsChoiceMenu(player)));
+		
+		return map;
+	}
 }
